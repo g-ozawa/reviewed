67b45ae4b1e7b2e073a7729ac86dbd66a3dc3cb4
ozw <ozawa@giftee.co>
2018-06-07 23:50:12 +0900

    remove label when dismissed

--- index.js
+++ index.js
@@ -6,6 +6,33 @@ const defaultConfig = {
 }
 
 module.exports = app => {
+  app.on('pull_request.synchronize', async context => {
+    const params = context.issue()
+
+    const config = await getConfig(context, 'reviewed.yml') || defaultConfig
+    const rmLabel = context.issue({ name: config.labelName })
+
+    const issueLabels = await context.github.issues.getIssueLabels(params)
+    const labels = issueLabels.data.map(label => label.name)
+
+    if (!labels.includes(config.labelName)) {
+      return
+    }
+
+    const reviews = await context.github.pullRequests.getReviews(params)
+    const requested_reviewers = await context.github.pullRequests.getReviewRequests(params)
+    const pull_request = await context.github.pullRequests.get(params)
+
+    const author = getPullRequestAuthor(pull_request)
+    const reviewers = getReviewers(reviews, requested_reviewers, author)
+    const approved_reviewers = getApprovedReviewers(reviews)
+
+    const isReviewed = approved_reviewers.length > 0 && reviewers.length === approved_reviewers.length
+    if (!isReviewed) {
+      context.github.issues.removeLabel(rmLabel)
+    }
+  });
+
   app.on('pull_request_review.submitted', async context => {
     const params = context.issue()
 
